58b7bbb6ecfd37aba3fed590d0b4af5cbcc05838
David Haslip <103548776+davidhaslip@users.noreply.github.com>
2024-11-19 06:45:09 -0800

    Update dashboard.js

--- public/dashboard.js
+++ public/dashboard.js
@@ -71,10 +71,10 @@ document.querySelectorAll('.add-item-button').forEach(button => {
                 <input type="text" placeholder="Label" class="new-item-label" />
                 <button class="save-item-button">Save</button>
             </td>
-            <td>
-                
-                <button class="save-item-button">cencel</button>
-            </td>
+            ${summaries.map(() => `
+                <td>
+                    <input type="number" value="0.00" disabled />
+                </td>`).join('')}
         `;
 
         buttonRow.parentNode.insertBefore(newRow, buttonRow);
