f5d695e73ff658079c4b19ac885a4f6bac252847
David Haslip <103548776+davidhaslip@users.noreply.github.com>
2024-11-22 07:07:22 -0800

    Update help.js

--- public/help.js
+++ public/help.js
@@ -1,10 +1,13 @@
 function toggleFaq(id) {
     const faqItem = document.getElementById(id);
-    const faqContent = faqItem.querySelector('.faq-content'); // Select the content element within the FAQ item
+    const faqContent = faqItem.querySelector('.faq-content');
+    const button = faqItem.querySelector('.view-more');
 
     if (faqContent.classList.contains('expanded')) {
         faqContent.classList.remove('expanded');
+        button.textContent = 'View More';
     } else {
         faqContent.classList.add('expanded');
+        button.textContent = 'View Less';
     }
 }
